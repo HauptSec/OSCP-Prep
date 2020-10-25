cc6da6edb5c5cbe303813be4ca1e72faee9dde89
Ahaupt3 <alexhaupt3@gmail.com>
2020-10-25 00:08:14 -0700

    Updated Setup script

--- Setup/Setup.sh
+++ Setup/Setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Link Python3/Pip3 to Python/Pip command
+sudo ln -s /usr/bin/python3 /usr/bin/python
+sudo apt install -y python3-pip
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+
 # Install Tools
 sudo cp ../Scripts/1-Recon/nmapAutomator.sh /usr/local/bin/nmapAutomator
-git -C ~/Tools clone https://github.com/maurosoria/dirsearch.git
+git -C ~/Tools clone https://github.com/maurosoria/dirsearch.git && cd dirsearch && mv dirsearch.py dirsearch && cd ..
+sudo python -m pip install git+https://github.com/Tib3rius/AutoRecon.git
+git -C ~/Tools clone https://github.com/codingo/Reconnoitre.git && cd Reconnoitre && sudo python setup.py install && cd ..
 
 # Update PATH
 cat PATH >> ~/.zshrc
