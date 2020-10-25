223e84fe6ba82e3fed6a13839099516ba7d18fa1
Ahaupt3 <alexhaupt3@gmail.com>
2020-10-24 21:27:43 -0700

    Updated Setup script

--- Setup/Setup.sh
+++ Setup/Setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Install Tools
-sudo curl https://raw.githubusercontent.com/Ahaupt3/OSCP-Prep/main/Scripts/1%20-%20Recon/nmapAutomator.sh -o /usr/bin/local/nmapAutomator
+sudo cp ../Scripts/1-Recon/nmapAutomator.sh /usr/local/bin/nmapAutomator
 git -C ~/Tools clone https://github.com/maurosoria/dirsearch.git
-sudo apt install -y proxychains4
 
 # Update PATH
-sudo curl https://raw.githubusercontent.com/Ahaupt3/OSCP-Prep/main/Setup/PATH >> ~/.zshrc
+cat PATH >> ~/.zshrc
 source ~/.zhsrc
