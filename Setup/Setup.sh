d79dc7454a34b71ab36905dae9209c4c2750a7cb
Ahaupt3 <alexhaupt3@gmail.com>
2020-11-04 03:54:55 -0800

    Updated Setup script

--- Setup/Setup.sh
+++ Setup/Setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-BOLD='\033[1m'
+GREEN='\033[0;1;32m'
 YELLOW='\033[0;33m'
 
 if [ ! -d ~/Tools ]; then
@@ -9,7 +8,7 @@ if [ ! -d ~/Tools ]; then
 fi
 
 # Link Python3/Pip3 to Python/Pip command
-echo -e "${GREEN}${BOLD}Linking Python3/Pip3 to Python/Pip${YELLOW}"
+echo -e "${GREEN}Linking Python3/Pip3 to Python/Pip${YELLOW}"
 sudo rm /usr/bin/python
 sudo ln -s /usr/bin/python3 /usr/bin/python
 sudo apt install -y python3-pip
@@ -18,37 +17,37 @@ sudo ln -s /usr/bin/pip3 /usr/bin/pip
 echo -e ""
 
 # Install Required Tools
-echo -e "${GREEN}${BOLD}Installing nmapAutomator${YELLOW}"
+echo -e "${GREEN}Installing nmapAutomator${YELLOW}"
 sudo curl https://raw.githubusercontent.com/Ahaupt3/OSCP-Prep/main/Scripts/1-Recon/nmapAutomator.sh -o /usr/local/bin/nmapAutomator && sudo chmod +x /usr/local/bin/nmapAutomator
 echo -e ""
 
-echo -e "${GREEN}${BOLD}Installing kickoffscans${YELLOW}"
+echo -e "${GREEN}Installing kickoffscans${YELLOW}"
 sudo curl https://raw.githubusercontent.com/Ahaupt3/OSCP-Prep/main/Scripts/1-Recon/kickoffscans.sh -o /usr/local/bin/kickoffscans && sudo chmod +x /usr/local/bin/kickoffscans
 echo -e ""
 
-echo -e "${GREEN}${BOLD}Installing dirsearch${YELLOW}"
+echo -e "${GREEN}Installing dirsearch${YELLOW}"
 git -C ~/Tools clone https://github.com/maurosoria/dirsearch.git && cd ~/Tools/dirsearch && mv dirsearch.py dirsearch && cd ~ || exit
 echo -e ""
 
-echo -e "${GREEN}${BOLD}Installing autorecon${YELLOW}"
+echo -e "${GREEN}Installing autorecon${YELLOW}"
 sudo python -m pip install git+https://github.com/Tib3rius/AutoRecon.git
 echo -e ""
 
-echo -e "${GREEN}${BOLD}Installing reconnoitre${YELLOW}"
+echo -e "${GREEN}Installing reconnoitre${YELLOW}"
 git -C ~/Tools clone https://github.com/codingo/Reconnoitre.git && cd ~/Tools/Reconnoitre && sudo python setup.py install && cd ~ || exit
 echo -e ""
 
 # Install Additional Tools
-echo -e "${GREEN}${BOLD}Installing evil-winrm${YELLOW}"
+echo -e "${GREEN}Installing evil-winrm${YELLOW}"
 sudo gem install evil-winrm
 echo -e ""
 
-echo -e "${GREEN}${BOLD}Installing proxychains4${YELLOW}"
+echo -e "${GREEN}Installing proxychains4${YELLOW}"
 sudo apt install -y proxychains4
 echo -e ""
 
 # Update PATH
-echo -e "${GREEN}${BOLD}Updating PATH${YELLOW}"
+echo -e "${GREEN}Updating PATH${YELLOW}"
 if [ -f .zshrc ]
 then
     echo "" >> ~/.zshrc
